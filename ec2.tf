fe1813c0aae09d406ae916f608eb3a92d90fd6fa
Norbert Majer <norbert.majer2@kingfisher.com>
2025-04-26 15:22:19 +0100

    major refactor

--- ec2.tf
+++ ec2.tf
@@ -5,30 +5,31 @@ resource "aws_default_vpc" "default" {
 }
 
 resource "aws_default_subnet" "default_az1" {
-  availability_zone = "eu-west-2a"
+  availability_zone = "us-east-1a"
 
   tags = {
-    Name = "Default subnet for eu-west-2a from terraform" # 172.31.16.0/20
+    Name = "Default subnet for us-east-1a from terraform" # 172.31.16.0/20
   }
 }
 
 
-data "aws_ami" "amzn-linux-2023-ami" {
-  most_recent = true
-  owners      = ["amazon"]
+# data "aws_ami" "amzn-linux-2023-ami" {
+#   most_recent = true
+#   owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["al2023-ami-2023.*-x86_64"]
-  }
-}
+#   filter {
+#     name   = "name"
+#     values = ["al2023-ami-2023.*-x86_64"]
+#   }
+# }
 
-resource "aws_instance" "sockets5" {
-  ami                    = data.aws_ami.amzn-linux-2023-ami.id
+resource "aws_instance" "cks_ec2" {
+  # ami                    = data.aws_ami.amzn-linux-2023-ami.id
+  ami                    = "ami-084568db4383264d4"
   instance_type          = "t3.nano"
   subnet_id              = aws_default_subnet.default_az1.id
-  key_name               = "socks5_proxy_key"
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  key_name               = "cks_ec2_key"
+  vpc_security_group_ids = [aws_security_group.allow_all_traffic.id]
 
   cpu_options {
     core_count       = 1
@@ -36,36 +37,44 @@ resource "aws_instance" "sockets5" {
   }
 
   tags = {
-    Name = "sockets5_proxy"
+    Name = "cks_ec2"
   }
 }
 
-resource "aws_key_pair" "socks5_key" {
-  key_name   = "socks5_proxy_key"
-  public_key = file("~/.ssh/socks5_proxy_key.pub")
+resource "aws_key_pair" "cks_ec2_key" {
+  key_name   = "cks_ec2_key"
+  public_key = file("~/.ssh/cks_ec2_key.pub")
 }
 
 output "instance_public_ip" {
   description = "The public IP address assigned to the instance, if applicable."
   value = try(
     # aws_eip.this[0].public_ip,
-    aws_instance.sockets5.public_ip,
+    aws_instance.cks_ec2.public_ip,
     # aws_instance.ignore_ami[0].public_ip,
     # aws_spot_instance_request.this[0].public_ip,
     null,
   )
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh_socks5_proxy"
-  description = "Allow SSH inbound traffic"
+resource "aws_security_group" "allow_all_traffic" {
+  name        = "allow_all_traffic_cks_ec2"
+  description = "Allow inbound traffic only from my IP"
   vpc_id      = aws_default_vpc.default.id
 
+  # ingress {
+  #   description = "SSH from your IP"
+  #   from_port   = 22
+  #   to_port     = 22
+  #   protocol    = "tcp"
+  #   cidr_blocks = [var.home_ip]
+  # }
+
   ingress {
-    description = "SSH from your IP"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    description = "All traffic from my IP only"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = [var.home_ip]
   }
 
@@ -77,6 +86,6 @@ resource "aws_security_group" "allow_ssh" {
   }
 
   tags = {
-    Name = "allow_ssh_socks5_proxy"
+    Name = "allow_all_traffic_cks_ec2"
   }
 }
