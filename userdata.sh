26d8def4b960c408732dfe894c29dc6c75a238fa
Norbert Majer <norbert.majer2@kingfisher.com>
2025-04-27 20:38:20 +0100

    create systemd

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# ETCD server creation & certificates
+# ETCD server & certificates creation
 apt-get update && apt-get -y install tcpdump net-tools
 mkdir /root/binaries
 cd /root/binaries
@@ -39,3 +39,31 @@ openssl x509 -req -in etcd.csr -CA ca.crt -CAkey ca.key -CAcreateserial  -out et
 openssl genrsa -out dudung.key 2048
 openssl req -new -key dudung.key -subj "/CN=dudung" -out dudung.csr
 openssl x509 -req -in dudung.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out dudung.crt -extensions v3_req  -days 2000
+
+# create systemd & start etcd running
+mkdir /var/lib/etcd
+chmod 700 /var/lib/etcd
+
+cat <<EOF | sudo tee /etc/systemd/system/etcd.service
+[Unit]
+Description=etcd
+Documentation=https://github.com/coreos
+
+[Service]
+ExecStart=/usr/local/bin/etcd \\
+  --cert-file=/root/certificates/etcd.crt \\
+  --key-file=/root/certificates/etcd.key \\
+  --trusted-ca-file=/root/certificates/ca.crt \\
+  --client-cert-auth \\
+  --listen-client-urls https://127.0.0.1:2379 \\
+  --advertise-client-urls https://127.0.0.1:2379 \\
+  --data-dir=/var/lib/etcd
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl start etcd
+systemctl enable etcd
